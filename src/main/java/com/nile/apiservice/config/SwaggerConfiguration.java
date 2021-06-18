69a2f206baf063cd8403fab46004612efcdbab12
newtein <newtein80@gmail.com>
2021-06-18 09:49:44 +0900

    Remove unused import

--- src/main/java/com/nile/apiservice/config/SwaggerConfiguration.java
+++ src/main/java/com/nile/apiservice/config/SwaggerConfiguration.java
@@ -1,18 +1,8 @@
 package com.nile.apiservice.config;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
-import com.fasterxml.classmate.TypeResolver;
-
 import org.springdoc.core.customizers.OpenApiCustomiser;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.swagger.v3.oas.models.OpenAPI;
@@ -22,17 +12,6 @@ import io.swagger.v3.oas.models.media.Content;
 import io.swagger.v3.oas.models.media.MediaType;
 import io.swagger.v3.oas.models.responses.ApiResponse;
 import io.swagger.v3.oas.models.responses.ApiResponses;
-
-// import springfox.documentation.builders.PathSelectors;
-// import springfox.documentation.builders.RequestHandlerSelectors;
-// import springfox.documentation.builders.ResponseMessageBuilder;
-// import springfox.documentation.schema.ModelRef;
-// import springfox.documentation.service.ApiInfo;
-// import springfox.documentation.service.Contact;
-// import springfox.documentation.service.ResponseMessage;
-// import springfox.documentation.spi.DocumentationType;
-// import springfox.documentation.spring.web.plugins.Docket;
-// import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @Configuration
 public class SwaggerConfiguration implements WebMvcConfigurer {
